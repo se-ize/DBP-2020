b0adbf863eff994e4645f37d9490fac82fbfcf75
se-ize <70623290+se-ize@users.noreply.github.com>
2020-11-02 21:14:15 +0900

    Update film_select.php

--- midterm/film_select.php
+++ midterm/film_select.php
@@ -45,6 +45,7 @@
             background-color: #F0DFF4;
             font-family: Concolas,m monospace;
             font-family: 12px;
+            text-align: center;
         }
         table{
             width: 100%;
